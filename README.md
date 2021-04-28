88b46bf79018cc0a7c693223f075cdcf9b640104
dh <dharri51@asu.edu>
2021-04-27 22:00:54 -0400

    Day 1 - Hello World in Haskell

---
+++ README.md
@@ -0,0 +1,6 @@
+# 30 days of Haskell
+
+I try to write some amount of Haskell every day for at least 30 days here.
+
+### To load file:
+``` ghci> :l <filename.hs>```
---
+++ day_1/day_1.hs
@@ -0,0 +1,5 @@
+hello :: String -> String
+hello name = "Hello, " ++ name
+
+helloGoodbye :: String -> String -> String
+helloGoodbye name1 name2 = "Hello " ++ name1 ++ ", Goodbye " ++ name2
